22723555860b045bfe0ce1886b57834bbff3d8ca
ASkiingrock <93888719+ASkiingrock@users.noreply.github.com>
2022-04-26 14:06:16 +1000

    First (and only?) commit

---
+++ Division.py
@@ -0,0 +1,85 @@
+import math
+# ignore this is just my rounding function
+# admittedly, this script does have some division in it, but these are trivial
+# this could be done by a human without division
+# we could easily round numbers for instace
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
+valid = False
+
+while not valid:
+    try:
+        total = int(input("The numerator (up to 999,999): "))
+        if total < 1000000 and total > 0:
+            valid = True
+        else:
+            print("\nPlease select a number between (inclusive) 1 - 999,999\n")
+    except ValueError:
+        print("\nPlease select a number between (inclusive) 1 - 999,999\n")
+    
+valid = False
+while not valid:
+    try:
+        m = int(input("\nThe denominator (up to 999,999): "))
+        if m < 1000000 and m > 0:
+            valid = True
+        else:
+            print("\nPlease select a number between (inclusive) 1 - 999,999\n")
+    except TypeError:
+        print("\nPlease select a number between (inclusive) 1 - 999,999\n")
+
+lb =  0
+ub = 1000000
+
+def whole(lb, ub):
+    # necessary division for binary search
+    # again, could be done by humans without division
+    # i would like to remove it, but to get a number halway between two i need it
+    # unless anyone can think of another way?
+    g = round(lb +(ub-lb)/2)
+    if g*m == total:
+        return g, g+1
+    # result found
+    elif ub-lb == 1:
+        return lb, ub
+    # lower and upper bound are one apart, need to do the next decimal place
+    elif g*m < total:
+        return whole(g, ub)
+    elif g*m > total:
+        return whole(lb, g)
+
+def firstPoint(lb, ub, decimal):
+    # necessary division for binary search
+    # again, could be done by humans without division
+    # i would like to remove it, but to get a number halway between two i need it
+    # unless anyone can think of another way?
+    g = round_half_up(lb +(ub-lb)/2, decimal)
+    if g*m == total:
+        return g, g+0.1
+    # result found
+    elif round_half_up(ub-lb, decimal+1) == 10**(-(decimal)):
+        return lb, ub
+    # lower and upper bound are one apart, need to do the next decimal place
+    elif g*m < total:
+        return firstPoint(g, ub, decimal)
+    elif g*m > total:
+        return firstPoint(lb, g, decimal)
+
+lb, ub = whole(0, total)
+for i in range(1, 14):              # this function can iteratively get more decimal places, 
+    lb,ub = firstPoint(lb, ub, i)   # which is the purpose for the custom rounding function 
+    if lb*m == total:               # to round to specific decimal places
+        result = lb
+        break
+    elif ub*m == total:
+        result = ub
+        break
+    elif i == 13:
+        result = round_half_up(ub, 13)
+        break
+    
+# for some reason I can only do it to a max of 13 digits otherwise it throws a recursion error
+print(f"\nThe result is (to 13 decimal places): {result}")
+
+
---
+++ readme.md
@@ -0,0 +1,43 @@
+-------------- DIVISION by binary search -------------
+
+So this is my division function without using division
+- It's a bit slow
+- No real need for it
+- Also the limit of the numbers you can use is just for ease and not bothering about calculating what will throw a recursion error
+
+Not admittedly, there is some division, but this is only for the rounding function (which is trivial) and the binary search
+So it's not useful at all
+
+How does it work?
+-----------------
+
+1) Get the number you want to divide (numerator), we'll call this T
+2) Get the number you want to divide by (denominator), we'll call this M
+3) It does binary search of the numbers up to T, to see what numbers are closest to, when multiplied by M, resuling in T
+4) This is accomplished through altering the upper and lower bounds and then guessing in the middle (binary search)
+5) This is accomplished using recursion
+6) Once he whole part of the number is calculated, then it iteratively runs the function again, rounding to smaller and smaller decimal places
+7) This goes up to 13 decimal places, as it throws a recursion error if I go any further
+
+For the square root file, instead of step 3, it checks whether the guess * guess == T
+
+How do I run it?
+----------------
+
+For division, run division.py
+For square roots, run sqrt.py
+
+Will this crash the housing market?
+-----------------------------------
+
+No, why would you ask that?
+
+Will you be coming to tour around my home city showing off your intellectual prowess?
+-------------------------------------------------------------------------------------
+
+Depends on what's in it for me
+
+Is this the end of he README?
+-----------------------------
+
+Yes.
